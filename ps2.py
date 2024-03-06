24a04e4d9aae0d60f47bb7ae4e7ebdbbd5202233
paynesc1 <paynesc1@gmail.com>
2024-03-06 12:11:15 -0700

    Added functions to hangman

--- ps2.py
+++ ps2.py
@@ -14,33 +14,39 @@ def choose_words(wordlist):
      return random.choice(wordlist)
 
 def is_word_guessed(secret_word, letters_guessed):
-    total = int(0)
-    length = int(len(secret_word))
-    # letters_guessed = secret_word
-    print(secret_word)
-    for letter in letters_guessed:
-        if letter in secret_word:
-            print(f"letter {letter} in word!")
-            length -= 1
-            print(length)
-            if length == 0:
-                return True
+    for letter in secret_word:
+        if letter not in letters_guessed:
+            return False
+    return True
+
+def get_guessed_word(secret_word, letters_guessed):
+    string = ""
+    for letter in secret_word:
+        if letter in letters_guessed:
+            string += letter
         else:
-            print(f"letter {letter} not in word.")
-            print("Add limb.")
-            total += 1
-            if total == length:
-                return False
-    return False
+            string += '_ '
+    return string
+
+def get_available_letters(letters_guessed):
+    my_string = ""
+    alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    for letter in alpha:
+        if letter in letters_guessed:
+            continue
+        else:
+            my_string += letter
+    return my_string
 
 
 
-        
-            
 
-
-wordlist = load_words()
-word = choose_words(wordlist)
-letters_guessed = ['a','t','e','o','v']
-print(is_word_guessed(word, letters_guessed))
+# wordlist = load_words()
+# word = choose_words(wordlist)
+word = "apple"
+# letters_guessed = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+letters_guessed = ['e', 'i', 'k', 'p', 'r', 's']
+# print(is_word_guessed(word, letters_guessed))
+# print(get_guessed_word(word, letters_guessed))
+print(get_available_letters(letters_guessed))
 
