98405ba177f4473d80bef995d45e1397b3afc73d
paynesc1 <paynesc1@gmail.com>
2024-03-09 12:20:36 -0700

    Added more Hangman code

--- ps2.py
+++ ps2.py
@@ -29,24 +29,45 @@ def get_guessed_word(secret_word, letters_guessed):
     return string
 
 def get_available_letters(letters_guessed):
+    #enter a letter
+    #if that letter is in alpha, take it out and return the rest of alpha
     my_string = ""
-    alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    alpha = 'abcdefghijklmnopqrstuvwxyz'
     for letter in alpha:
-        if letter in letters_guessed:
-            continue
-        else:
+        if letter not in letters_guessed:
             my_string += letter
     return my_string
 
 
 
+def hangman(word):
+    length = len(word)
+    guesses = 6
+    print(word)
+    letters_guessed = []
+    while guesses > 0:
+        print(f"The word contains {length} letters!")
+        print(f"You have {guesses} guesses left!")
+        print("Available letters: ",(get_available_letters(letters_guessed)))
+        guess = (input("Guess a letter... "))
+        if guess in letters_guessed:
+            print("Letter already guessed, try again!")
+        else:
+            letters_guessed.append(guess)
+            if guess not in word:
+                guesses -= 1
+            else:
+                print("That letter is in the word")
+                print(get_guessed_word(word, letters_guessed))
 
-# wordlist = load_words()
-# word = choose_words(wordlist)
-word = "apple"
+
+wordlist = load_words()
+word = choose_words(wordlist)
+# word = "apple"
 # letters_guessed = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 letters_guessed = ['e', 'i', 'k', 'p', 'r', 's']
 # print(is_word_guessed(word, letters_guessed))
 # print(get_guessed_word(word, letters_guessed))
-print(get_available_letters(letters_guessed))
+# print(get_available_letters(letters_guessed))
+print(hangman(word))
 
