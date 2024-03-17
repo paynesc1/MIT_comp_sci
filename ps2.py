d9d99c3f622078829990da85b7d50474aee36860
paynesc1 <paynesc1@gmail.com>
2024-03-17 13:02:15 -0600

    Hangman

--- ps2.py
+++ ps2.py
@@ -43,22 +43,54 @@ def get_available_letters(letters_guessed):
 def hangman(word):
     length = len(word)
     guesses = 6
+    warnings = 3
     print(word)
     letters_guessed = []
     while guesses > 0:
         print(f"The word contains {length} letters!")
         print(f"You have {guesses} guesses left!")
         print("Available letters: ",(get_available_letters(letters_guessed)))
+        print()
         guess = (input("Guess a letter... "))
+        guess = guess.lower()
         if guess in letters_guessed:
             print("Letter already guessed, try again!")
+        elif len(guess) > 1:
+            print("You can only guess once, try again.")
+            warnings -= 1
+            print(f"***{warnings} warnings left!***")
+            if warnings == 0:
+                guesses -= 1
+                print("Lost a guess!")
+                warnings = 3
+                continue
         else:
             letters_guessed.append(guess)
-            if guess not in word:
+            if not guess.isalpha():
+                print("Character not in alphabet, try again!")
+                warnings -= 1
+                print(f"***{warnings} warnings left!***")
+                if warnings == 0:
+                    guesses -= 1
+                    print("Lost a guess!")
+                    warnings = 3
+                    continue
+            elif guess not in word:
+                print("That letter is NOT in the word, try again!")
                 guesses -= 1
             else:
                 print("That letter is in the word")
                 print(get_guessed_word(word, letters_guessed))
+        if guesses == 0:
+            print("----------")
+            print("Better luck next time!")
+            print(f"The word was {word}!")
+        if is_word_guessed(word, letters_guessed) == False:
+            continue
+        else:
+            print("  ** ** You guessed it!  **  **")
+            print(f"Guesses left: {guesses}")
+            break
 
 
 wordlist = load_words()
